246ced4f0fd6e1bce34a30f2402b1fe7e2d46b0a
isakawecki <isakawecki@gmail.com>
2025-04-01 13:11:00 -0300

    Documentando

--- tarefas/src/main/java/com/lista/tarefas/services/UsuarioService.java
+++ tarefas/src/main/java/com/lista/tarefas/services/UsuarioService.java
@@ -13,15 +13,18 @@ public class UsuarioService {
 	
 	@Autowired 
 	UsuarioRepository usuarioRepository;
-	
+
+	    // Método para inserir um novo usuário 
 	public UsuarioDTO inserir(UsuarioDTO dto) {
 		Usuario usuario = new Usuario();
 	
 		usuario.setNome(dto.getNome());
 	    usuario.setEmail(dto.getEmail());
 	
-		
+		 // Salva o usuário no banco de dados e obtém o usuário salvo
 		usuario = usuarioRepository.save(usuario);
+
+		 // Retorna um DTO com as informações do usuário recém-criado
 		return new UsuarioDTO(usuario);
 	
 	
